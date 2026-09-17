bcdea9da554c1e1070af85e77ee8219fe98c0cc1
Carlosvf23 <carlos_svf23@gmail.com>
2026-09-17 15:53:52 -0300

    Migrar proyecto a Spring Boot y estructura Maven estandar

---
+++ src/main/java/cl/carlos/banco/BancoConsolidacionApplication.java
@@ -0,0 +1,13 @@
+package cl.carlos.banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoConsolidacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoConsolidacionApplication.class, args);
+	}
+
+}
